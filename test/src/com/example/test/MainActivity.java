1119a3d176ee16be756da4afe6ece4989a6477bd
Grzegorz Stepniewski <gstepniewski@gmail.com>
2013-11-04 09:27:03 +0100

    Lab 2 - Added simple controls and overrides
    Added a button to main layout
    Clicking the button switches text on the TextView
    Added alternative resources for landscape orientation

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.test;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		
+		Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(this);
     }
 
 
@@ -19,5 +26,39 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		TextView tv = (TextView) findViewById(R.id.textview);
+		
+		String hello = getResources().getString(R.string.hello_world);
+		String bye = getResources().getString(R.string.bye_world);
+		
+		if (hello.equals(tv.getText())) {
+			tv.setText(bye);
+		} else {
+			tv.setText(hello);
+		}
+	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		TextView tv = (TextView) findViewById(R.id.textview);
+		String text = tv.getText().toString();
+		
+		outState.putString("textviewtext", text);
+	}
+	
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		
+		TextView tv = (TextView) findViewById(R.id.textview);
+		String text = savedInstanceState.getString("textviewtext");
+		
+		tv.setText(text);
+	}
     
 }
