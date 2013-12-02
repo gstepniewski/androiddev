02abcb19daef20c177deaef93c8426fbdc6e6c2b
Grzegorz Stepniewski <gstepniewski@gmail.com>
2013-12-02 10:32:26 +0100

    Notification added

--- test/src/com/example/test/MainActivity.java
+++ test/src/com/example/test/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.test;
 
 import android.net.Uri;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
@@ -12,7 +16,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -43,6 +46,9 @@ public class MainActivity extends Activity implements OnClickListener {
 		
 		TextView button7 = (TextView) findViewById(R.id.button7);
 		button7.setOnClickListener(mSQLiteClickListener);
+		
+		TextView button8 = (TextView) findViewById(R.id.button8);
+		button8.setOnClickListener(mNotiClickListener);
 
 		TextView tv = (TextView) findViewById(R.id.textview);
 		SharedPreferences sp = getSharedPreferences("mysharedprefences", MODE_PRIVATE);
@@ -143,7 +149,25 @@ public class MainActivity extends Activity implements OnClickListener {
 		public void onClick(View v) {
 			Intent intent = new Intent(MainActivity.this, SQLiteActivity.class);
 			startActivity(intent);
+		}
+	};
+	
+	private OnClickListener mNotiClickListener = new OnClickListener() {
+		@SuppressWarnings("deprecation")
+		@Override
+		public void onClick(View v) {
+			NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 			
+			String title = "Tytul";
+			String content = "content content content content content content content content content";
+			String ticker = "ticker ticker ticker ticker ticker ticker ticker ticker ticker";
+			
+			Notification noti = new Notification(R.drawable.ic_launcher, ticker, System.currentTimeMillis());
+			PendingIntent npi = PendingIntent.getService(MainActivity.this, 0, new Intent(), 0);
+			
+			noti.setLatestEventInfo(MainActivity.this, title, content, npi);
+			
+			nm.notify(0, noti);
 		}
 	};
 	
